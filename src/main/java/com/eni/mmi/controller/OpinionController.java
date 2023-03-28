6a482e95f31027273bfbe5066110db62efbc6261
corentinsolal <corentinsolal@gmail.com>
2023-03-28 17:10:32 +0200

    Fixes

--- src/main/java/com/eni/mmi/controller/OpinionController.java
+++ src/main/java/com/eni/mmi/controller/OpinionController.java
@@ -15,53 +15,53 @@ import javax.servlet.http.HttpSession;
 @Controller
 @RequestMapping("/opinions")
 public class OpinionController {
-	private MovieService movieService;
+    private MovieService movieService;
 
-	@Autowired
-	public OpinionController(MovieService movieService) {
-		this.movieService = movieService;
-	}
+    @Autowired
+    public OpinionController(MovieService movieService) {
+        this.movieService = movieService;
+    }
 
-	// Création d'un nouvel avis
-	@GetMapping("/add")
-	public String createOpinion(@RequestParam("id")long idMovie, Model model, HttpSession session) {
-		// Test si un membre est connecté
-		Object att = session.getAttribute("userSession");
-		if (att != null) {
-			//Mise en session du film sélectionné
-			session.setAttribute("selectedMovie", movieService.getMovieById(idMovie));
-			Opinion opinion = new Opinion();
-			// Ajout de l'instance dans le modèle
-			model.addAttribute("opinion", opinion);
+    // Création d'un nouvel avis
+    @GetMapping("/add/{id}")
+    public String createOpinion(@PathVariable(value = "id") long idMovie, Model model, HttpSession session) {
+        // Test si un membre est connecté
+        Object att = session.getAttribute("userSession");
+        if (att != null) {
+            //Mise en session du film sélectionné
+            session.setAttribute("selectedMovie", movieService.getMovieById(idMovie));
+            Opinion opinion = new Opinion();
+            // Ajout de l'instance dans le modèle
+            model.addAttribute("opinion", opinion);
 
-			return "opinion";
-		} else {
-			return "redirect:/login";
-		}
-	}
+            return "opinion";
+        } else {
+            return "redirect:/login";
+        }
+    }
 
-	// Récupération de l'objet opinion du formulaire
-	// sauvegarde
-	@PostMapping("/add")
-	public String createOpinion(@ModelAttribute("opinion") Opinion opinion, Model model, HttpSession session) {
-		// Test si un membre est connecté
-		Object att = session.getAttribute("userSession");
-		if (att != null) {
-			try {
-				//Associé le membre à l'avis
-				Member m =(Member) att;
-				opinion.setMember(m);
-				System.out.println(opinion);
-				//Récupération du film en session :
-				Movie selectedMovie = (Movie) session.getAttribute("selectedMovie");
-				movieService.saveOpinion(opinion, selectedMovie);
-				return "redirect:/movies";
-			} catch (MovieException e) {
-				model.addAttribute("errors", e.getErrors());
-				return "opinion";
-			}
-		} else {
-			return "redirect:/login";
-		}
-	}
+    // Récupération de l'objet opinion du formulaire
+    // sauvegarde
+    @PostMapping("/add")
+    public String createOpinion(@ModelAttribute("opinion") Opinion opinion, Model model, HttpSession session) {
+        // Test si un membre est connecté
+        Object att = session.getAttribute("userSession");
+        if (att != null) {
+            try {
+                //Associé le membre à l'avis
+                Member m = (Member) att;
+                opinion.setMember(m);
+                System.out.println(opinion);
+                //Récupération du film en session :
+                Movie selectedMovie = (Movie) session.getAttribute("selectedMovie");
+                movieService.saveOpinion(opinion, selectedMovie);
+                return "redirect:/movies";
+            } catch (MovieException e) {
+                model.addAttribute("errors", e.getErrors());
+                return "opinion";
+            }
+        } else {
+            return "redirect:/login";
+        }
+    }
 }
